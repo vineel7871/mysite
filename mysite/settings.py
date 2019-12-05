77ce8fa844b58810a7cdb4da4c2bedc248b564d2
vineel <vineel7871@outlook.com>
2019-12-05 22:32:43 +0530

    added django-heroku module

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,13 +11,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
-# import dj_database_url
+# import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -95,8 +93,8 @@ DATABASES = {
 }
 
 
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # local database settings
 # DATABASES = {
